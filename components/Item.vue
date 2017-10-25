ab69d4f04c599f6d716f08100a5e075ae3392a92
Sam Zhao <zhao.shengjun@gmail.com>
2017-10-25 16:12:17 +1000

    url filter and show data if exists

--- components/Item.vue
+++ components/Item.vue
@@ -1,19 +1,26 @@
 <template>
   <div class="code">
     <ul class="list pa2">
-      <li v-for="item in items" @key="item.id" class="item">
+      <li v-for="item in items" @key="item.id" class="item f6">
         <div class="score">
           {{item.score}}
         </div>
-        <div class="title">
+        <div class="title ">
           {{item.title}}
+          <template v-if="item.url">
+            <a class="f7" :href="item.url">{{item.url | hostname}}</a>
+          </template>
         </div>
         <div class="details">
-          by {{item.by}} {{item.time | timeSince}} ago
+          by {{item.by}}
+          <p class="ma0 i f7">{{item.time | timeSince}} ago</p>
         </div>
-        <div class="comments">
-          {{item.descendants}} comments
-        </div>
+        <template v-if="item.descendants">
+          <div class="comments">
+            {{item.descendants}} comments
+          </div>
+
+        </template>
       </li>
     </ul>
   </div>
--- plugins/filter.js
+++ plugins/filter.js
@@ -1,7 +1,9 @@
 import Vue from "vue";
 import { distanceInWordsToNow } from "date-fns";
+import { parse } from "url";
 
 Vue.filter("timeSince", timestamp => {
   const timeInMS = timestamp * 1000;
   return distanceInWordsToNow(timeInMS);
 });
+Vue.filter("hostname", url => parse(url).hostname.replace(/^www\./, ""));
