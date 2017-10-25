d56c1b54dec5e242ff561fd98b78047eb1a35c72
Sam Zhao <zhao.shengjun@gmail.com>
2017-10-25 15:29:30 +1000

    use layout to create navigation

---
+++ pages/show.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Show</h1>
+    <Items></Items>
+  </div>
+</template>
+
+<script>
+import Items from "../components/Item.vue";
+export default {
+  components: { Items },
+  async fetch({ store }) {
+    await store.dispatch("LOAD_ITEMS", "newstories");
+  }
+};
+</script>
