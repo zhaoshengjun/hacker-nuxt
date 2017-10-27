a974ad34f083ba0f5072b324e09f2c08cf56e994
Sam Zhao <zhao.shengjun@gmail.com>
2017-10-27 10:03:55 +1000

    user page

---
+++ pages/user/_id.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="pa4 code">
+    {{user.id}} has been a user {{user.created | timeSince}}.
+  </div>
+</template>
+
+<script>
+import axios from "../../plugins/axios";
+export default {
+  async asyncData({ route }) {
+    const userId = route.params.id;
+    const response = await axios.get(`/user/${userId}`);
+    return { user: response.data };
+  }
+};
+</script>
